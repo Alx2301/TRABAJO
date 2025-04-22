3cd705022d86b32fa8a824eea1351b7f22a3c021
Alx2301 <aleska730@gmail.com>
2025-04-22 18:18:45 +0200

    Copia 4

--- Assets/Menu principal/Scripts/EntradaSalidaAnimacion.cs
+++ Assets/Menu principal/Scripts/EntradaSalidaAnimacion.cs
@@ -14,6 +14,7 @@ public class EntradaSalidaAnimacion : MonoBehaviour
     {
         animator.Play("PANEL SALIR 2");
     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
